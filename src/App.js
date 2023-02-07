231c1702e1535f1bc0e26650d53d8c59257f7fff
Palak Tiwari <palaktiwari@CJ-134.local>
2023-02-07 04:15:49 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
+import StarRating from './components/StarRating';
 import './App.css';
 
-function App() {
+const App = () => {
+  // Get the rating from a db if required.
+  // The value 3 is just for testing.
+  const [rating, setRating] = useState(3);
+
+  const handleChange = (value) => {
+    setRating(value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container main">
+      <h1>STAR RATING DEMO</h1>
+
+     <StarRating 
+       count={5}
+       size={50}
+       value={rating}
+       activeColor ={'red'}
+       inactiveColor={'#ddd'}
+       onChange={handleChange}  />
+
     </div>
-  );
+  )
 }
 
 export default App;
+
+
+
+
---
+++ src/components/StarRating.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const StarRating = ({count, value, inactiveColor='#ddd', size=24, activeColor='#f00', onChange}) => {
+
+  // short trick 
+  const stars = Array.from({length: count}, () => '✰')
+
+  // Internal handle change function
+  const handleChange = (value) => {
+    onChange(value + 1);
+  }
+
+  return (
+    <div>
+      {stars.map((s, index) => {
+        let style = inactiveColor;
+        if (index < value) {
+          style=activeColor;
+        }
+        return (
+          <span className="star" 
+            key={index}
+            style={{color: style, width:size, height:size, fontSize: size}}
+            onClick={()=>handleChange(index)}>{s}</span>
+        )
+      })}
+      {value}
+    </div>
+  )
+}
+
+
+export default StarRating;
+
